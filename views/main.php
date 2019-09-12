eeafe23c02340100a53a485c85b40f5c22086015
franlima <fol@cesar.school>
2019-09-12 08:15:00 -0400

    Remmoving bootstrap

--- views/main.php
+++ views/main.php
@@ -8,42 +8,131 @@
 	<title>Lotosystem</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
   <!--<link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/semantic/semantic.min.css">-->
-  <link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/bootstrap.min.css">
+  <!--<link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/bootstrap.min.css">-->
+  <style type="text/css">
+
+.hidden.menu {
+  display: none;
+}
+
+.masthead.segment {
+  min-height: 700px;
+  padding: 1em 0em;
+}
+.masthead .logo.item img {
+  margin-right: 1em;
+}
+.masthead .ui.menu .ui.button {
+  margin-left: 0.5em;
+}
+.masthead h1.ui.header {
+  margin-top: 3em;
+  margin-bottom: 0em;
+  font-size: 4em;
+  font-weight: normal;
+}
+.masthead h2 {
+  font-size: 1.7em;
+  font-weight: normal;
+}
+
+.ui.vertical.stripe {
+  padding: 8em 0em;
+}
+.ui.vertical.stripe h3 {
+  font-size: 2em;
+}
+.ui.vertical.stripe .button + h3,
+.ui.vertical.stripe p + h3 {
+  margin-top: 3em;
+}
+.ui.vertical.stripe .floated.image {
+  clear: both;
+}
+.ui.vertical.stripe p {
+  font-size: 1.33em;
+}
+.ui.vertical.stripe .horizontal.divider {
+  margin: 3em 0em;
+}
+
+.quote.stripe.segment {
+  padding: 0em;
+}
+.quote.stripe.segment .grid .column {
+  padding-top: 5em;
+  padding-bottom: 5em;
+}
+
+.footer.segment {
+  padding: 5em 0em;
+}
+
+.secondary.pointing.menu .toc.item {
+  display: none;
+}
+
+@media only screen and (max-width: 700px) {
+  .ui.fixed.menu {
+    display: none !important;
+  }
+  .secondary.pointing.menu .item,
+  .secondary.pointing.menu .menu {
+    display: none;
+  }
+  .secondary.pointing.menu .toc.item {
+    display: block;
+  }
+  .masthead.segment {
+    min-height: 350px;
+  }
+  .masthead h1.ui.header {
+    font-size: 2em;
+    margin-top: 1.5em;
+  }
+  .masthead h2 {
+    margin-top: 0.5em;
+    font-size: 1.5em;
+  }
+}
+</style>
 </head>
 <body class="pushable">
-  <!--
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">Lotosystem 
-            <?php if(isset($_SESSION['is_logged_in'])) : ?>
-              <em><?php echo 'Ola ' .$_SESSION['user_data']['username']; ?></em>
-            <?php endif; ?>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </div>
-    </nav>
-    -->
+    <div class="ui vertical inverted sidebar menu left">
+      <a class="active item">Home</a>
+      <?php if(isset($_SESSION['is_logged_in'])) : ?>
+        <a class="item" href="<?php echo ROOT_URL; ?>operations">Operations</a>
+        <a class="item" href="<?php echo ROOT_URL; ?>reports">Reports</a>
+      <?php endif; ?>
+      <?php if(isset($_SESSION['is_logged_in'])) : ?>
+        <a class="item" href="<?php echo ROOT_URL; ?>users/logout">Logout</a>
+      <?php else : ?>
+        <a class="item" href="<?php echo ROOT_URL; ?>users/login">Login</a>
+      <?php endif; ?>
+    </div>
     <div class="pusher">
-      <div class="ui large secondary inverted pointing menu">
-        <a class="toc item">
-          <i class="sidebar icon"></i>
-        </a>
-        <a class="active item" href="<?php echo ROOT_URL; ?>">Home</a>
-        <?php if(isset($_SESSION['is_logged_in'])) : ?>
-          <a class="item" href="<?php echo ROOT_URL; ?>operations">Operations</a>
-          <a class="item" href="<?php echo ROOT_URL; ?>reports">Reports</a>
-        <?php endif; ?>
-        <?php if(isset($_SESSION['is_logged_in'])) : ?>
-          <a class="item" href="<?php echo ROOT_URL; ?>users/logout">Logout</a>
-        <?php else : ?>
-          <a class="item" href="<?php echo ROOT_URL; ?>users/login">Login</a>
-        <?php endif; ?>
-      </div>
-      <div class="jumbotron jumbotron-fluid">
-     	  <?php require($view); ?>
-        <?php Messages::display(); ?>
+      <div class="ui inverted vertical masthead center aligned segment">
+        <div class="ui container">
+          <div class="ui large secondary inverted pointing menu">
+            <a class="toc item">
+              <i class="sidebar icon"></i>
+            </a>
+            <a class="active item" href="<?php echo ROOT_URL; ?>">Home</a>
+            <?php if(isset($_SESSION['is_logged_in'])) : ?>
+              <a class="item" href="<?php echo ROOT_URL; ?>operations">Operations</a>
+              <a class="item" href="<?php echo ROOT_URL; ?>reports">Reports</a>
+            <?php endif; ?>
+            <?php if(isset($_SESSION['is_logged_in'])) : ?>
+              <a class="item" href="<?php echo ROOT_URL; ?>users/logout">Logout</a>
+            <?php else : ?>
+              <a class="item" href="<?php echo ROOT_URL; ?>users/login">Login</a>
+            <?php endif; ?>
+          </div>
+        </div>
+        <div class="ui text container">
+          <?php require($view); ?>
+          <?php Messages::display(); ?>
+        </div>
       </div>
     </div>
     <!-- Optional JavaScript -->
@@ -53,9 +142,31 @@
     <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
     <!--<script src="<?php echo ROOT_URL; ?>assets/js/semantic/semantic.min.js"></script>-->
     <script src="<?php echo ROOT_URL; ?>assets/js/popper.min.js"></script>
-    <script src="<?php echo ROOT_URL; ?>assets/js/bootstrap.min.js"></script>
+    <script>
+  $(document)
+    .ready(function() {
+      // fix menu when passed
+      $('.masthead')
+        .visibility({
+          once: false,
+          onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+          }
+        })
+      ;
+
+      // create sidebar and attach to menu open
+      $('.ui.sidebar')
+        .sidebar('attach events', '.toc.item')
+      ;
+    })
+  ;
+  </script>
 </body>
-<footer style="text-align: center">
-  <p class="mt-5 mb-3 text-muted">© 2018 Copyright:<a href=""> Loteria Lotoplaza</a></p>
+<footer style="ui inverted header">
+  <p class="">© 2018 Copyright:<a href=""> Loteria Lotoplaza</a></p>
 </footer>
 </html>
